1b0e72679ca23569368c20f35a096057cde6ff61
Jerome Gravel-Niquet <jeromegn@gmail.com>
2024-03-04 13:38:46 -0500

    remove structopt entirely, we're using this programmatically

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -4,23 +4,15 @@ extern crate log;
 use std::env;
 use std::error::Error;
 use std::process::exit;
-use structopt::StructOpt;
 
-use nperf_core::{
-    args,
-    cmd_collate,
-    cmd_csv,
-    cmd_metadata,
-    cmd_record,
-    cmd_trace_events
-};
+use nperf_core::{args, cmd_collate, cmd_csv, cmd_metadata, cmd_record, cmd_trace_events};
 
 #[cfg(feature = "inferno")]
 use nperf_core::cmd_flamegraph;
 
-fn main_impl() -> Result< (), Box< dyn Error > > {
-    if env::var( "RUST_LOG" ).is_err() {
-        env::set_var( "RUST_LOG", "info" );
+fn main_impl() -> Result<(), Box<dyn Error>> {
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "info");
     }
 
     #[cfg(feature = "env_logger")]
@@ -28,31 +20,31 @@ fn main_impl() -> Result< (), Box< dyn Error > > {
 
     let opt = args::Opt::from_args();
     match opt {
-        args::Opt::Record( args ) => {
+        args::Opt::Record(args) => {
             if args.profiler_args.panic_on_partial_backtrace {
-                warn!( "Will panic on partial backtraces!" );
-                if env::var( "RUST_BACKTRACE" ).is_err() {
-                    env::set_var( "RUST_BACKTRACE", "1" );
+                warn!("Will panic on partial backtraces!");
+                if env::var("RUST_BACKTRACE").is_err() {
+                    env::set_var("RUST_BACKTRACE", "1");
                 }
             }
 
-            cmd_record::main( args )?;
-        },
+            cmd_record::main(args)?;
+        }
         #[cfg(feature = "inferno")]
-        args::Opt::Flamegraph( args ) => {
-            cmd_flamegraph::main( args )?;
-        },
-        args::Opt::Csv( args ) => {
-            cmd_csv::main( args )?;
-        },
-        args::Opt::Collate( args ) => {
-            cmd_collate::main( args )?;
-        },
-        args::Opt::Metadata( args ) => {
-            cmd_metadata::main( args )?;
-        },
-        args::Opt::TraceEvents( args ) => {
-            cmd_trace_events::main( args )?;
+        args::Opt::Flamegraph(args) => {
+            cmd_flamegraph::main(args)?;
+        }
+        args::Opt::Csv(args) => {
+            cmd_csv::main(args)?;
+        }
+        args::Opt::Collate(args) => {
+            cmd_collate::main(args)?;
+        }
+        args::Opt::Metadata(args) => {
+            cmd_metadata::main(args)?;
+        }
+        args::Opt::TraceEvents(args) => {
+            cmd_trace_events::main(args)?;
         }
     }
 
@@ -60,8 +52,8 @@ fn main_impl() -> Result< (), Box< dyn Error > > {
 }
 
 fn main() {
-    if let Err( error ) = main_impl() {
-        eprintln!( "error: {}", error );
-        exit( 1 );
+    if let Err(error) = main_impl() {
+        eprintln!("error: {}", error);
+        exit(1);
     }
 }
--- src/args.rs
+++ src/args.rs
@@ -1,5 +1,4 @@
 use std::ffi::OsString;
-use structopt::StructOpt;
 
 use perf_event_open::EventSource;
 
@@ -56,39 +55,16 @@ pub enum TargetProcess {
     ByNameWaiting(String, u64),
 }
 
-#[derive(StructOpt, Clone, Debug)]
-#[structopt(rename_all = "kebab-case")]
+#[derive(Clone, Debug)]
 pub struct ProcessFilter {
     /// Profiles a process with a given PID (conflicts with --process)
-    #[structopt(
-        long,
-        short = "p",
-        raw(required_unless_one = r#"&[
-            "process"
-        ]"#)
-    )]
     pub pid: Option<u32>,
     /// Profiles a process with a given name (conflicts with --pid)
-    #[structopt(
-        long,
-        short = "P",
-        raw(required_unless_one = r#"&[
-            "pid"
-        ]"#)
-    )]
     pub process: Option<String>,
     /// Will wait for the profiled process to appear
-    #[structopt(
-        long,
-        short = "w",
-        raw(conflicts_with = r#"
-            "pid"
-        "#)
-    )]
     pub wait: bool,
     /// Specifies the number of seconds which the profiler should wait
     /// for the process to appear; makes sense only when used with the `--wait` option
-    #[structopt(long, default_value = "60")]
     pub wait_timeout: u32,
 }
 
@@ -108,7 +84,7 @@ impl From<ProcessFilter> for TargetProcess {
     }
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, Debug, StructOpt)]
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub enum Granularity {
     Address,
     Function,
@@ -130,247 +106,159 @@ fn parse_granularity(value: &str) -> Granularity {
     }
 }
 
-#[derive(StructOpt, Debug)]
-#[structopt(rename_all = "kebab-case")]
+#[derive(Debug)]
 pub struct GenericProfilerArgs {
     /// The file to which the profiling data will be written
-    #[structopt(long, short = "o", parse(from_os_str))]
     pub output: Option<OsString>,
 
     /// The number of samples to gather; unlimited by default
-    #[structopt(long)]
     pub sample_count: Option<u64>,
 
     /// Determines for how many seconds the measurements will be gathered
-    #[structopt(long, short = "l")]
     pub time_limit: Option<u64>,
 
     /// Prevents anything in the profiler's address space from being swapped out; might increase memory usage significantly
-    #[structopt(long)]
     pub lock_memory: bool,
 
     /// Disable online backtracing
-    #[structopt(long)]
     pub offline: bool,
 
-    #[structopt(long, raw(hidden = "true"))]
     pub panic_on_partial_backtrace: bool,
 
-    #[structopt(flatten)]
     pub process_filter: ProcessFilter,
 }
 
-#[derive(StructOpt, Debug)]
-#[structopt(rename_all = "kebab-case")]
+#[derive(Debug)]
 pub struct RecordArgs {
     /// The frequency with which the measurements will be gathered
-    #[structopt(long, short = "F", default_value = "900")]
     pub frequency: u32,
 
     /// The source of perf events
-    #[structopt(
-        long,
-        short = "s",
-        parse(from_str = "parse_event_source"),
-        raw(possible_values = r#"&[
-            "hw_cpu_cycles",
-            "hw_ref_cpu_cycles",
-            "sw_cpu_clock",
-            "sw_page_faults",
-            "sw_dummy"
-        ]"#)
-    )]
     pub event_source: Option<EventSource>,
 
     /// Size of the gathered stack payloads (in bytes)
-    #[structopt(long, default_value = "64512")]
     pub stack_size: u32,
 
     /// Gather data but do not do anything with it; useful only for testing
-    #[structopt(long)]
     pub discard_all: bool,
 
-    #[structopt(flatten)]
     pub profiler_args: GenericProfilerArgs,
 }
 
-#[derive(StructOpt, Debug)]
-#[structopt(rename_all = "kebab-case")]
+#[derive(Debug)]
 pub struct SharedCollationArgs {
     /// A file or directory with extra debugging symbols; can be specified multiple times
-    #[structopt(long, short = "d", parse(from_os_str))]
     pub debug_symbols: Vec<OsString>,
 
     /// A path to a jitdump file
-    #[structopt(long, parse(from_os_str))]
     pub jitdump: Option<OsString>,
 
-    #[structopt(long, raw(hidden = "true"))]
     pub force_stack_size: Option<u32>,
 
-    #[structopt(long, raw(hidden = "true"))]
     pub omit: Vec<String>,
 
-    #[structopt(long, raw(hidden = "true"))]
     pub only_sample: Option<u64>,
 
     /// Completely ignores kernel callstacks
-    #[structopt(long)]
     pub without_kernel_callstacks: bool,
 
     /// Only process the samples generated *after* this many seconds after launch.
-    #[structopt(long)]
     pub from: Option<String>,
 
     /// Only process the samples generated *before* this many seconds after launch.
-    #[structopt(long)]
     pub to: Option<String>,
 
     /// The input file to use; record it with the `record` subcommand
-    #[structopt(parse(from_os_str))]
     pub input: OsString,
 }
 
-#[derive(StructOpt, Debug)]
-#[structopt(rename_all = "kebab-case")]
+#[derive(Debug)]
 pub struct ArgMergeThreads {
     /// Merge callstacks from all threads
-    #[structopt(long)]
     pub merge_threads: bool,
 }
 
-#[derive(StructOpt, Debug)]
-#[structopt(rename_all = "kebab-case")]
+#[derive(Debug)]
 pub struct ArgGranularity {
     /// Specifies at what granularity the call frames will be merged
-    #[structopt(
-        long,
-        default_value = "function",
-        parse(from_str = "parse_granularity"),
-        raw(possible_values = r#"&[
-            "address",
-            "function",
-            "line"
-        ]"#)
-    )]
     pub granularity: Granularity,
 }
 
 #[cfg(feature = "inferno")]
-#[derive(StructOpt, Debug)]
-#[structopt(rename_all = "kebab-case")]
+#[derive(Debug)]
 pub struct FlamegraphArgs {
-    #[structopt(flatten)]
     pub collation_args: SharedCollationArgs,
 
-    #[structopt(flatten)]
     pub arg_merge_threads: ArgMergeThreads,
 
-    #[structopt(flatten)]
     pub arg_granularity: ArgGranularity,
 
     /// The file to which the flamegraph will be written to (instead of the stdout)
-    #[structopt(long, short = "o", parse(from_os_str))]
     pub output: Option<OsString>,
 }
 
-#[derive(StructOpt, Debug)]
-#[structopt(rename_all = "kebab-case")]
+#[derive(Debug)]
 pub struct CsvArgs {
-    #[structopt(flatten)]
     pub collation_args: SharedCollationArgs,
 
     /// The sampling interval, in seconds
-    #[structopt(long, short = "t")]
     pub sampling_interval: Option<f64>,
 
     /// The file to which the CSV will be written to (instead of the stdout)
-    #[structopt(long, short = "o", parse(from_os_str))]
     pub output: Option<OsString>,
 }
 
-#[derive(StructOpt, Debug)]
-#[structopt(rename_all = "kebab-case")]
+#[derive(Debug)]
 pub struct TraceEventsArgs {
-    #[structopt(flatten)]
     pub collation_args: SharedCollationArgs,
 
-    #[structopt(flatten)]
     pub arg_granularity: ArgGranularity,
 
-    #[structopt(long)]
     pub absolute_time: bool,
 
     /// The sampling period; samples within one sampling period will be merged together
-    #[structopt(long, short = "p", parse(from_str = "parse_period"))]
     pub period: Option<u64>,
 
     /// The file to which the trace events will be written to
-    #[structopt(long, short = "o", parse(from_os_str))]
     pub output: OsString,
 }
 
-#[derive(StructOpt, Debug)]
-#[structopt(rename_all = "kebab-case")]
+#[derive(Debug)]
 pub struct CollateArgs {
-    #[structopt(flatten)]
     pub collation_args: SharedCollationArgs,
 
-    #[structopt(flatten)]
     pub arg_merge_threads: ArgMergeThreads,
 
-    #[structopt(flatten)]
     pub arg_granularity: ArgGranularity,
 
     /// Selects the output format
-    #[structopt(
-        long,
-        default_value = "collapsed",
-        parse(from_str = "parse_collate_format"),
-        raw(possible_values = r#"&[
-            "collapsed",
-            "perf-like"
-        ]"#)
-    )]
     pub format: CollateFormat,
 }
 
-#[derive(StructOpt, Debug)]
-#[structopt(rename_all = "kebab-case")]
+#[derive(Debug)]
 pub struct MetadataArgs {
     /// The input file to use; record it with the `record` subcommand
-    #[structopt(parse(from_os_str))]
     pub input: OsString,
 }
 
-#[derive(StructOpt, Debug)]
-#[structopt(
-    raw(author = "\"Jan Bujak <j@exia.io>\""),
-    raw(setting = "structopt::clap::AppSettings::ArgRequiredElseHelp")
-)]
+#[derive(Debug)]
 pub enum Opt {
     /// Records profiling information with perf_event_open
-    #[structopt(name = "record")]
     Record(RecordArgs),
 
     /// Emits an SVG flamegraph
     #[cfg(feature = "inferno")]
-    #[structopt(name = "flamegraph")]
     Flamegraph(FlamegraphArgs),
 
     /// Emits a CSV file
-    #[structopt(name = "csv")]
     Csv(CsvArgs),
 
     /// Emits trace events for use with Chromium's Trace Viewer
-    #[structopt(name = "trace-events")]
     TraceEvents(TraceEventsArgs),
 
     /// Emits collated stack traces for use with Brendan Gregg's flamegraph script
-    #[structopt(name = "collate")]
     Collate(CollateArgs),
 
     /// Outputs rudimentary JSON-formatted metadata
-    #[structopt(name = "metadata")]
     Metadata(MetadataArgs),
 }
